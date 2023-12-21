e50a599d35d7592bb11e59639b25f73004ec85ef
aaron <aaronlyn88@gmail.com>
2023-12-21 14:35:24 +0800

    feat: initialize gradle project

---
+++ src/main/webapp/jsp/hello.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello Page</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
+
---
+++ src/test/java/com/edpa/epda/EpdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edpa.epda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
